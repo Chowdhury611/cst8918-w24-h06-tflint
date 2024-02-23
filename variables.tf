e6c3bf1e26da1286303fd434245f885c48c50a14
ChowdhurySalehaFerdowsy <shipu9300@gmail.com>
2024-02-22 22:28:58 -0500

    steps done

--- variables.tf
+++ variables.tf
@@ -1,11 +1,13 @@
 # Define config variables
-variable "labelPrefix" {
+variable "label_prefix" {
   type        = string
   description = "Your college username. This will form the beginning of various resource names."
 }
 
 variable "region" {
-  default = "westus3"
+  type       = string
+  default    = "westus3"
+  description = "this is a rigion"
 }
 
 variable "admin_username" {
